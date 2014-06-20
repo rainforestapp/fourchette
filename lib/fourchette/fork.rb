dbe9dbd013020e8650c7fca64c7d840e91f8f28f
Jean-Philippe Boily <j@jipi.ca>
2014-06-20 14:24:18 -0700

    move the comment with test URL to a method
    Reason for that is so that we can override it if we don’t want that
    comment, which is our case, as we also have another comment with our
    custom domains, too.

--- lib/fourchette/fork.rb
+++ lib/fourchette/fork.rb
@@ -62,9 +62,13 @@ class Fourchette::Fork
   def create_unless_exists
     unless @heroku.app_exists?(fork_name)
       @heroku.fork(ENV['FOURCHETTE_HEROKU_APP_TO_FORK'] ,fork_name)
-      # Update PR with URL
-      @github.comment_pr(pr_number, "Test URL: #{@heroku.client.app.info(fork_name)['web_url']}")
+      post_fork_url
     end
+  end
+
+  def post_fork_url
+    # Update PR with URL
+    @github.comment_pr(pr_number, "Test URL: #{@heroku.client.app.info(fork_name)['web_url']}")
   end
 
   def git_branch_name
