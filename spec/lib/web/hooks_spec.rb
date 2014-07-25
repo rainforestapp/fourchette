192777b4cfb35bde5097fa60ca1d6ae7bd4ad3f0
Jean-Philippe Boily <j@jipi.ca>
2014-07-25 10:54:41 -0400

    code style change

--- spec/lib/web/hooks_spec.rb
+++ spec/lib/web/hooks_spec.rb
@@ -5,8 +5,7 @@ require "sucker_punch/testing/inline"
 describe "GitHub web hooks receiver" do
   it "kicks an async job doing all the work" do
     expected_param = { "something" => "ok" }
-    Fourchette::PullRequest
-      .any_instance
+    Fourchette::PullRequest.any_instance
       .should_receive(:perform)
       .with(expected_param)
 
