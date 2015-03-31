dbc3295e02afa5ecad273d0cf4175c7964805dba
Tommy Fisher <tommy@carbonfive.com>
2015-03-31 09:54:42 -0700

    Fixing specs

--- lib/fourchette/pgbackups.rb
+++ lib/fourchette/pgbackups.rb
@@ -18,6 +18,7 @@ class Fourchette::Pgbackups
 
     pg_client =  Heroku::Client::HerokuPostgresql.new(attachment)
 
+    logger.info 'Waiting for database #{to_name} to become available'
     wait_for_db(pg_client)
 
     pg_client.pg_copy(from_name, from_url, to_name, to_url)
@@ -27,7 +28,6 @@ class Fourchette::Pgbackups
 
   # taken from Heroku::Command::Pg
   def wait_for_db(client)
-    logger.info 'Waiting for database to become available...'
     loop do
       status = client.get_wait_status
       if status[:error?]
@@ -37,12 +37,6 @@ class Fourchette::Pgbackups
       end
       break unless status[:waiting?]
       sleep 5
-    end
-  end
-
-  def existing_backups?(heroku_app_name)
-    @heroku.client.addon.list(heroku_app_name).any? do |addon|
-      addon['name'] == 'pgbackups'
     end
   end
 
--- spec/lib/fourchette/pgbackups_spec.rb
+++ spec/lib/fourchette/pgbackups_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
+require 'heroku/helpers/heroku_postgresql'
+
 describe Fourchette::Pgbackups do
   describe '#copy' do
     let(:from_app_name) { 'awesome app' }
     let(:to_app_name) { 'awesomer app!' }
     let(:pg_backup) { Fourchette::Pgbackups.new }
     let(:heroku) { instance_double(Fourchette::Heroku) }
+    let(:pg_client) { instance_double(Heroku::Client::HerokuPostgresql) }
 
     let(:config_vars_from) do
       { 'HEROKU_POSTGRESQL_FROM_URL' => 'postgres://from...',
@@ -18,11 +21,16 @@ describe Fourchette::Pgbackups do
     end
 
     before do
+      response = double('response')
       allow(Fourchette::Heroku).to receive(:new).and_return(heroku)
 
       allow(heroku)
-        .to receive_message_chain(:client, :addon, :list)
-        .and_return([{ 'name' => addon_name }])
+        .to receive_message_chain(:legacy_client, :get_attachments)
+        .and_return(response)
+
+      allow(response)
+        .to receive(:body)
+        .and_return(['raw_attachment'])
 
       allow(heroku)
         .to receive(:config_vars)
@@ -33,46 +41,31 @@ describe Fourchette::Pgbackups do
         .to receive(:config_vars)
         .with(to_app_name)
         .and_return(config_vars_to)
+
+      allow(pg_client)
+        .to receive(:get_wait_status)
+        .and_return({ :waiting? => false })
     end
 
-    context 'when Pgbackups addon is enabled' do
-      let(:addon_name) { 'pgbackups' }
-      let(:backup) { instance_double(Heroku::Client::Pgbackups) }
+    it 'copies the pg database from origin app to destination one' do
+      expect(Heroku::Helpers::HerokuPostgresql::Attachment)
+        .to receive(:new)
+        .with('raw_attachment')
+        .and_return('attachment')
 
-      it 'launches a PG backup from origin app to destination one' do
-        expect(Heroku::Client::Pgbackups)
-          .to receive(:new)
-          .with(config_vars_from['PGBACKUPS_URL'] + '/api')
-          .and_return(backup)
+      expect(Heroku::Client::HerokuPostgresql)
+        .to receive(:new)
+        .with('attachment')
+        .and_return(pg_client)
 
-        expect(backup).to receive(:create_transfer).with(
-          config_vars_from['HEROKU_POSTGRESQL_FROM_URL'],
-          'HEROKU_POSTGRESQL_FROM_URL',
-          config_vars_to['HEROKU_POSTGRESQL_TO_URL'],
-          'HEROKU_POSTGRESQL_TO_URL'
-        )
+      expect(pg_client).to receive(:pg_copy).with(
+        'FROM',
+        config_vars_from['HEROKU_POSTGRESQL_FROM_URL'],
+        'TO',
+        config_vars_to['HEROKU_POSTGRESQL_TO_URL']
+      )
 
-        pg_backup.copy(from_app_name, to_app_name)
-      end
-    end
-
-    context 'when Pgbackups addon is not enabled' do
-      let(:addon_name) { 'addon' }
-
-      it 'enables Pgbackups addon and launches a PG backup' do
-        expect(heroku)
-          .to receive_message_chain(:client, :addon, :create)
-          .with(to_app_name, { plan: 'pgbackups' })
-
-        expect(heroku)
-          .to receive_message_chain(:client, :addon, :create)
-          .with(from_app_name, { plan: 'pgbackups' })
-
-        expect(Heroku::Client::Pgbackups)
-          .to receive_message_chain(:new, :create_transfer)
-
-        pg_backup.copy(from_app_name, to_app_name)
-      end
+      pg_backup.copy(from_app_name, to_app_name)
     end
   end
 end
